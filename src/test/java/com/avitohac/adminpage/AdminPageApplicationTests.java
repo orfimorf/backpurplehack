9386c100ab11ebdaa9cf508c84ced44c3a0f5f53
daniil <garipov.daniil@urfu.me>
2024-03-09 19:34:24 +0500

    spring init

---
+++ src/test/java/com/avitohac/adminpage/AdminPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avitohac.adminpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a741c626d8379e1b57332131974c23b43f87abb7
daniil <garipov.daniil@urfu.me>
2024-03-10 18:11:37 +0500

    Сегменты, локации и микрокатегории

---
+++ entities/User.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(id, location_id, microcategory_id) {
+        this._id = id
+        this._location_id = location_id
+        this._microcategory_id = microcategory_id
+    }
+
+    async findCost() {
+        
+    }
+}

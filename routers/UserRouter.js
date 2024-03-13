9ead71a7aae3aacf2f0486621c088b5249705b79
daniil <garipov.daniil@urfu.me>
2024-03-14 04:37:33 +0500

    Убрал клиентские функции

--- routers/UserRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const userController = require('../controllers/UserController')
-
-const router = new Router()
-
-router.get('/getCost', userController.getCost)
-
-module.exports = router
--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,10 @@
 const Router = require('express')
 
-const userRouter = require('./UserRouter')
 const matrixRouter = require('./MatrixRouter')
 const mainRouter = require('./MainRouter')
 
 const router = new Router()
 
-
-router.use('/user', userRouter)
 router.use('/matrix', matrixRouter)
 router.use('/main', mainRouter)
 

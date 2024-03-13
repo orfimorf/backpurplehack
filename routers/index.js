603f71866574b87807a1ede7f16798976ffe1fae
orfimorf <ilya.lapushkin@mail.ru>
2024-03-14 03:31:03 +0500

    fix

--- routers/index.js
+++ routers/index.js
@@ -1,14 +1,12 @@
 const Router = require('express')
-const baselineRouter= require('./BaselineRouter')
-const discountRouter = require('./DiscountRouter')
+
 const userRouter = require('./UserRouter')
 const matrixRouter = require('./MatrixRouter')
 const mainRouter = require('./MainRouter')
 
 const router = new Router()
 
-router.use('/baseline', baselineRouter)
-router.use('/discount', discountRouter)
+
 router.use('/user', userRouter)
 router.use('/matrix', matrixRouter)
 router.use('/main', mainRouter)
